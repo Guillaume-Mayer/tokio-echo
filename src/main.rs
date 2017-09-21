44d693ba2f4de4fdeea6f1b1b9e9c63f30ce2450
Guillaume Mayer <gmayer@arkho.tech>
2017-09-21 12:47:15 -0300

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,16 @@
+extern crate bytes;
+extern crate futures;
+extern crate tokio_io;
+extern crate tokio_proto;
+extern crate tokio_service;
+
+use std::io;
+use std::str;
+use bytes::BytesMut;
+use tokio_io::codec::{Encoder, Decoder};
+
+pub struct LineCodec;
+
+fn main() {
+    println!("Hello, world!");
+}
